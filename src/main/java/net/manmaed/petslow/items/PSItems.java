be8cdb856e1a40e35774264bf0dd5d2562f0588e
manmaed <manmaed18@gmail.com>
2021-10-12 23:41:40 +0100

    Old Item code removed

--- src/main/java/net/manmaed/petslow/items/PSItems.java
+++ src/main/java/net/manmaed/petslow/items/PSItems.java
@@ -20,30 +20,3 @@ public class PSItems {
     public static final RegistryObject<Item> CLAY_BREW = ITEMS.register("clay_brew", () -> new ClayBrew(new Item.Properties().tab(PetSlow.PETSLOW).stacksTo(1)));
 
 }
-
-//public static Item itemname;
-    /*public static Item mug;
-    public static Item spawnslow;
-    public static Item slowbrew;
-    public static Item claybrew;
-
-    public static void load() {
-        //Init'ing
-        //itemname = new ItemClass();
-        spawnslow = new SlowSpawn("spawnslow");
-        mug = new Mug("mug");
-        slowbrew = new SlowBrew("slowbrew");
-        claybrew = new ClayBrew("claybrew");
-        //beer = new SlowDrinkItemBase("beer");
-
-
-        //Registering
-        //MainClass.getRegistryHelper().registerItem(itemname);
-        PetSlow.getRegistryHelper().registerItem(spawnslow);
-        PetSlow.getRegistryHelper().registerItem(mug);
-        PetSlow.getRegistryHelper().registerItem(slowbrew);
-        PetSlow.getRegistryHelper().registerItem(claybrew);
-
-        //itemname.setRegistryName("itemname");
-        *//*spawnslow.setRegistryName("spawnslow");
-    }*/
