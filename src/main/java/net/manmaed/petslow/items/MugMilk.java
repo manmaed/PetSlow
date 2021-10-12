6549e9af79cfe29d7d0607330db576e5c6258061
manmaed <manmaed18@gmail.com>
2021-10-12 23:42:25 +0100

    Mug O Moo moo juice!

--- src/main/java/net/manmaed/petslow/items/MugMilk.java
+++ src/main/java/net/manmaed/petslow/items/MugMilk.java
@@ -1,8 +1,72 @@
 package net.manmaed.petslow.items;
 
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.*;
+import net.minecraft.world.World;
+
 /**
  * Created by manmaed on 12/10/2021.
  */
-public class MugMilk {
-    //TODO: MAYBE MILK?
+public class MugMilk extends Item {
+    public MugMilk(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        super.finishUsingItem(stack, worldIn, entityLiving);
+        if (entityLiving instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) entityLiving;
+            CriteriaTriggers.CONSUME_ITEM.trigger(player, stack);
+            player.awardStat(Stats.ITEM_USED.get(this));
+        }
+        if (!worldIn.isClientSide) {
+            entityLiving.curePotionEffects(stack);
+        }
+        if (stack.isEmpty()) {
+            return new ItemStack(PSItems.MUG.get());
+        } else {
+            if (entityLiving instanceof PlayerEntity && !((PlayerEntity) entityLiving).abilities.instabuild) {
+                ItemStack itemStack = new ItemStack(PSItems.MUG.get());
+                PlayerEntity player = (PlayerEntity) entityLiving;
+                if (!player.inventory.add(itemStack)) {
+                    player.drop(itemStack, false);
+                }
+            }
+            return stack;
+        }
+    }
+
+    /**
+     * How long it takes to use or consume an item
+     */
+    public int getUseDuration(ItemStack stack) {
+        return 32;
+    }
+
+    public UseAction getUseAnimation(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    @Override
+    public SoundEvent getDrinkingSound() {
+        return SoundEvents.GENERIC_DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatingSound() {
+        return SoundEvents.GENERIC_DRINK;
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        return DrinkHelper.useDrink(world, player, hand);
+    }
 }
