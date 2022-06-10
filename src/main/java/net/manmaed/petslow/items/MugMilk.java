fc8e6dbb35e14071e8e2988d9aa74f4016516375
manmaed <manmaed18@gmail.com>
2022-06-11 00:12:11 +0100

    Fix Mug o' Milk not removing effects

--- src/main/java/net/manmaed/petslow/items/MugMilk.java
+++ src/main/java/net/manmaed/petslow/items/MugMilk.java
@@ -6,10 +6,13 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.extensions.IForgeMobEffect;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -23,13 +26,14 @@ public class MugMilk extends Item {
         super(properties);
     }
 
+
+
     public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity) {
-        if (!level.isClientSide)
-            livingEntity.curePotionEffects(itemStack); // FORGE - move up so stack.shrink does not turn stack into air
-        if (livingEntity instanceof ServerPlayer) {
-            ServerPlayer serverplayer = (ServerPlayer) livingEntity;
-            CriteriaTriggers.CONSUME_ITEM.trigger(serverplayer, itemStack);
-            serverplayer.awardStat(Stats.ITEM_USED.get(this));
+        ItemStack milk_bucket = Items.MILK_BUCKET.getDefaultInstance(); //Hacky Bypass!
+        if (!level.isClientSide) livingEntity.curePotionEffects(milk_bucket); // FORGE - move up so stack.shrink does not turn stack into air
+        if (livingEntity instanceof ServerPlayer serverPlayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverPlayer, itemStack);
+            serverPlayer.awardStat(Stats.ITEM_USED.get(this));
         }
 
         if (livingEntity instanceof Player && !((Player) livingEntity).getAbilities().instabuild) {
